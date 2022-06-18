bc51caac8a866e65addf532a32528abd8e9be195
Mateusz <mateusz.nawrat7@gmail.com>
2022-06-18 10:05:07 +0200

    init new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-import Banner from "./Components/Banner/Banner";
-import About from "./Components/About/About";
-import Footer from "./Components/Footer/Footer";
-import Services from "./Components/Services/Services";
-
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Banner />
-        <About />
-        <Services />
-        <Footer />
-      </Fragment>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+      </header>
+    </div>
   );
 }
 
